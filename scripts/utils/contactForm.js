4e04ba5264ad0c1aee43ba3cc8502a7a409cf40d
inesder <inesderraz@hotmail.fr>
2024-03-22 15:19:49 +0100

    create fields and accessibility of the modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,16 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    modal.focus();
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+document.addEventListener('keydown', function(event){
+    if (event.key === "Escape"){
+        closeModal();
+    }
+});
