71c329a0e2a64903e540994d2ff6f6439d5fdbe4
inesder <inesderraz@hotmail.fr>
2024-03-25 14:04:13 +0100

    console log of input form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -14,3 +14,21 @@ document.addEventListener('keydown', function(event){
         closeModal();
     }
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche la soumission standard du formulaire
+
+    // Récupère les valeurs des champs du formulaire
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // Affiche les valeurs dans la console
+    console.log("Prénom:", firstname);
+    console.log("Nom:", lastname);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    closeModal(); // Ferme la modale après la soumission
+});
